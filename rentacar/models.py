060d961417c526b11b46692b46fb99ffe0743bf5
Yusuf <kurakiorg@gmail.com>
2023-06-15 21:42:00 +0300

    Stable Vers

--- rentacar/models.py
+++ rentacar/models.py
@@ -2,16 +2,17 @@ from django.db import models
 from django.contrib.auth.models import User
 from datetime import date
 from django.core.exceptions import ValidationError
-from django.core.validators import MinValueValidator
 
-class FixModel(models.Model): #i will use this fields for every model
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+class FixModel(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
     create_date = models.DateTimeField(auto_now_add=True)
     update_date = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        abstract = True
 
 class Car(FixModel):
-    plate_number = models.CharField(max_length=24,unique=True)
+    plate_number = models.CharField(max_length=24, unique=True)
     brand = models.CharField(max_length=24)
     model = models.CharField(max_length=24)
     year = models.PositiveIntegerField()
@@ -22,20 +23,12 @@ class Car(FixModel):
     ]
 
     gear = models.BooleanField(choices=GEAR_CHOICES)
-
-    rent_per_day = models.DecimalField(max_digits=5, decimal_places=2, validators=[MinValueValidator(1)])
+    rent_per_day = models.CharField(max_length=24)
     availability = models.BooleanField(default=True)
 
     def __str__(self):
         return f'{self.plate_number} - {self.brand}'
     
-    # def save(self, *args, **kwargs):
-    #     if self.reservation is not None:
-    #         self.availability = False
-    #     else:
-    #         self.availability = True
-    #     super(Car, self).save(*args, **kwargs)
-
 class Reservation(FixModel):
     car = models.ForeignKey(Car, on_delete=models.CASCADE)
     start_date = models.DateField()
