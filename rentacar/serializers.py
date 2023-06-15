599980cbeb656c0aedb958dba6e2d7e2667d0cab
Yusuf <kurakiorg@gmail.com>
2023-06-15 20:31:47 +0300

    FixSerializer

--- rentacar/serializers.py
+++ rentacar/serializers.py
@@ -1,12 +1,20 @@
 from rest_framework import serializers
 from .models import Car,Reservation
 
-class CarSerializers(serializers.ModelSerializer):
+class FixSerializer(serializers.ModelSerializer):
+    user = serializers.StringRelatedField()
+    user_id = serializers.IntegerField(required=False, read_only=True)
+
+    def create(self, validated_data):
+        validated_data['user_id'] = self.context['request'].user.id
+        return super().create(validated_data)
+
+class CarSerializers(FixSerializer):
     class Meta:
         model = Car
         fields = '__all__'
 
-class ReservationSerializers(serializers.ModelSerializer):
+class ReservationSerializers(FixSerializer):
     class Meta:
         model = Reservation
         fields = '__all__'
